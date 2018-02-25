02133bcd2301d43caf37a9d4f527ade583c58ec6
uclaseo <inseok.ucla@gmail.com>
2018-02-25 13:27:11 -0800

    Design on login page

--- tripo-front-end/src/Login/Login.jsx
+++ tripo-front-end/src/Login/Login.jsx
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FlatButton from 'material-ui/FlatButton';
+import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login-box">
+        <Link to="/landing">
+          <FlatButton label="Login" />
+        </Link>
+      </div>
+      <div className="signup-box">
+        <Link to="/landing">
+          <FlatButton label="Signup" />
+        </Link>
+      </div>
     </div>
   )
 }
